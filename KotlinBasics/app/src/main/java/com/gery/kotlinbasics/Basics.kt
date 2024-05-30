be03c96f9bf4ab917d65fc9b0ef01522729bf069
Geryenko Hawsen <gery.zarons@gmail.com>
2024-05-30 13:25:35 +0900

    28. Day 2 - Float and Double

--- KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Basics.kt
@@ -2,9 +2,7 @@ package com.gery.kotlinbasics
 
 fun main() {
 
-    // Byte, Short, Int, Long
-    var myAge : Long = 250123123123132123
-    println(myAge)
-    myAge = 1231232132132132132
-    println(myAge)
+    var pi = 3.14
+    println(pi)
+    pi = 3.141311231232131
 }
