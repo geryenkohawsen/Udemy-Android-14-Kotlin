fe80f8d1696718d299bb93e733a3022391addfb5
Geryenko Hawsen <gery.zarons@gmail.com>
2024-05-30 13:19:34 +0900

    27. Day 2 - Int, Long, Short, Byte and specifying the type manually

--- KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Basics.kt
@@ -2,8 +2,9 @@ package com.gery.kotlinbasics
 
 fun main() {
 
-    var myAge = 25
+    // Byte, Short, Int, Long
+    var myAge : Long = 250123123123132123
     println(myAge)
-    myAge = 28
+    myAge = 1231232132132132132
     println(myAge)
 }
