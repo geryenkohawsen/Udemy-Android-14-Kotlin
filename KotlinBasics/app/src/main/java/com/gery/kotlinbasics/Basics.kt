aafb51f4f6f25c9f795f43c664adde120b57674c
Geryenko Hawsen <gery.zarons@gmail.com>
2024-06-03 01:49:34 +0900

    31. Day 2 - Char, Unicode and Backslash escape character

--- KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Basics.kt
@@ -2,11 +2,8 @@ package com.gery.kotlinbasics
 
 fun main() {
 
-    val myTrue = true
-    val myFalse = false
-
-    println(myTrue || myFalse)
-    println(myTrue && myFalse)
-    println(!myTrue)
-    println(!myFalse)
+    val myChar = '\u00AE'
+    val myChar2 = '\\'
+    
+    println(myChar)
 }
