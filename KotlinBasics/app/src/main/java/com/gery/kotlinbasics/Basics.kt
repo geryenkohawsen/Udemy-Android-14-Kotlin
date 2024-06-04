708efd96770f817b96c7fdf549b049c1522fee37
Geryenko Hawsen <gery.zarons@gmail.com>
2024-06-05 01:33:51 +0900

    37. Day 2 - Rock Paper Scissors Part 2 - Finding out the winner

--- KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Basics.kt
@@ -9,12 +9,19 @@ fun main() {
 
     val randomNumber = (1..3).random()
 
-    if (randomNumber == 1) {
-        computerChoice = "Rock"
-    } else if (randomNumber == 2) {
-        computerChoice = "Paper"
-    } else if (randomNumber == 3) {
-        computerChoice = "Scissor"
+    when (randomNumber) {
+        1 -> computerChoice = "Rock"
+        2 -> computerChoice = "Paper"
+        3 -> computerChoice = "Scissor"
     }
     println(computerChoice)
+
+    val winner = when {
+        playerChoice == computerChoice -> "Tie"
+        playerChoice == "Rock" && computerChoice == "Scissor" -> "Player"
+        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+        playerChoice == "Scissor" && computerChoice == "Paper" -> "Player"
+        else -> "Computer"
+    }
+
 }
