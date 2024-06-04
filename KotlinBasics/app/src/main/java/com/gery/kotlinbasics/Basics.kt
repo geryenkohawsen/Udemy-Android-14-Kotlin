ffc78168f4b69ac17dfa2160fc8b7624da43c420
Geryenko Hawsen <gery.zarons@gmail.com>
2024-06-05 02:00:26 +0900

    39. Day 2 - While loops with counters

--- KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Basics.kt
@@ -2,32 +2,12 @@ package com.gery.kotlinbasics
 
 fun main() {
 
-    var computerChoice = ""
-    var playerChoice = ""
-    println("Rock, Paper, or Scissor? PICK!!")
-    playerChoice = readln()
-
-    val randomNumber = (1..3).random()
-
-    when (randomNumber) {
-        1 -> computerChoice = "Rock"
-        2 -> computerChoice = "Paper"
-        3 -> computerChoice = "Scissor"
-    }
-    println(computerChoice)
-
-    val winner = when {
-        playerChoice == computerChoice -> "Tie"
-        playerChoice == "Rock" && computerChoice == "Scissor" -> "Player"
-        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
-        playerChoice == "Scissor" && computerChoice == "Paper" -> "Player"
-        else -> "Computer"
+    var count = 0
+    while (count < 5) {
+        println("It is $count")
+        count++
     }
 
-    if (winner == "Tie") {
-        println("It's a tie")
-    }  else {
-        println("$winner won!!")
-    }
+    print("Loop finished!")
 
 }
