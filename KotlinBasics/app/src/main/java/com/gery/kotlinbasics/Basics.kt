9b0e7c8390da8cc170f6f1a1e2b2b8f375c6d414
Geryenko Hawsen <gery.zarons@gmail.com>
2024-06-06 01:12:13 +0900

    46. Day 3 - Quiz Challenge Solution - 1 Spoon 2 Spoons

--- KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Basics.kt
@@ -5,6 +5,10 @@ fun main() {
     makeCoffee(25)
 }
 
-fun makeCoffee(sugarCount : Int) {
-    println("Make coffee with $sugarCount")
+fun makeCoffee(sugarCount: Int) {
+    if (sugarCount == 1) {
+        println("Make coffee with $sugarCount spoon of sugar")
+    } else {
+        println("Make coffee with $sugarCount spoons of sugar")
+    }
 }
