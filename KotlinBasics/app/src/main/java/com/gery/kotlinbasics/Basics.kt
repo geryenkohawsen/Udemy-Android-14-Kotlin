8ce03d85e79001b2386a7e084f717a5c59c15373
Geryenko Hawsen <gery.zarons@gmail.com>
2024-06-06 01:14:57 +0900

    47. Day 3 - Quiz Challenge Solution - Who is the coffee for

--- KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Basics.kt
@@ -1,14 +1,21 @@
 package com.gery.kotlinbasics
 
 fun main() {
-    makeCoffee(1)
-    makeCoffee(25)
+    makeCoffee(1, "test1")
+    makeCoffee(0, "test2")
+    makeCoffee(25, "test3")
 }
 
-fun makeCoffee(sugarCount: Int) {
-    if (sugarCount == 1) {
-        println("Make coffee with $sugarCount spoon of sugar")
-    } else {
-        println("Make coffee with $sugarCount spoons of sugar")
+fun makeCoffee(sugarCount: Int, name: String) {
+    when (sugarCount) {
+        1 -> {
+            println(message = "Make coffee with $sugarCount spoon of sugar for $name")
+        }
+        0 -> {
+            println("Make black coffee for $name")
+        }
+        else -> {
+            println("Make coffee with $sugarCount spoons of sugar for $name")
+        }
     }
 }
