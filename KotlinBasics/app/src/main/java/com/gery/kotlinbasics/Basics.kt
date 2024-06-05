020083e414145975a03f60a9b309eab6b1548431
Geryenko Hawsen <gery.zarons@gmail.com>
2024-06-06 02:23:44 +0900

    50. Day 3 - Quiz Challenge Solution - Get User Input to Add two numbers

--- KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Basics.kt
@@ -1,8 +1,17 @@
 package com.gery.kotlinbasics
 
 fun main() {
-    val myResult = add(5, 10)
-    println(myResult)
+
+    println("Enter number 1")
+    val num1Str = readln()
+    val num1Int = num1Str.toInt()
+    println("Enter number 2")
+    val num2Str = readln()
+    val num2Int = num2Str.toInt()
+
+
+    val myResult = add(num1Int, num2Int)
+    println("The result is $myResult")
 }
 
 fun add(num1: Int, num2: Int): Int {
