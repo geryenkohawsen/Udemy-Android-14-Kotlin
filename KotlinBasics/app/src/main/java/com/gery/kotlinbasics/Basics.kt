ca5eddaedfb8fe1c9a2b5768e4eddc29a140ba8d
Geryenko Hawsen <gery.zarons@gmail.com>
2024-06-09 01:36:22 +0900

    54. Day 3 - Creating our First Class and Object

--- KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Basics.kt
@@ -2,55 +2,6 @@ package com.gery.kotlinbasics
 
 fun main() {
 
-    println("Enter number 1")
-    val num1Str = readln()
-//    val num1Int = num1Str.toInt()
-    val num1D = num1Str.toDouble()
-    println("Enter number 2")
-    val num2Str = readln()
-//    val num2Int = num2Str.toInt()
-    val num2D = num2Str.toDouble()
-
-
-//    val myResult = add(num1Int, num2Int)
-    val myResult = divide(num1D, num2D)
-    println("The result is $myResult")
-}
-
-fun divide(num1: Double, num2: Double): Double {
-    val result = num1 / num2
-    return result
-}
-
-fun add(num1: Int, num2: Int): Int {
-    val result = num1 + num2
-    return result
-}
-
-
-fun askCoffeeDetails() {
-    println("Who is the coffee for?")
-    val name = readln()
-
-    println("How much sugar?")
-    val sugarCountInput = readln()
-    val sugarCountInt = sugarCountInput.toInt()
-
-    makeCoffee(sugarCountInt, name)
-}
-
-fun makeCoffee(sugarCount: Int, name: String) {
-    when (sugarCount) {
-        1 -> {
-            println(message = "Make coffee with $sugarCount spoon of sugar for $name")
-        }
-
-        0 -> {
-            println("Make black coffee for $name")
-        }
-
-        else -> {
-            println("Make coffee with $sugarCount spoons of sugar for $name")
-        }
-    }
+    // Creating an object (instance) of the class dog
+    var daisy = Dog()
 }
---
+++ KotlinBasics/app/src/main/java/com/gery/kotlinbasics/Dog.kt
@@ -0,0 +1,13 @@
+package com.gery.kotlinbasics
+
+class Dog {
+
+    // A code that will execute when an instance of a class is created
+    init {
+        bark()
+    }
+
+    fun bark() {
+        println("Woof woof")
+    }
+}
