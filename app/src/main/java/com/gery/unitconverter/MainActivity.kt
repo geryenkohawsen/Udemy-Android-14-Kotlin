2c7d20dfc2ec987b44ae0fbf457575ce12c5d26b
Geryenko Hawsen <gery.zarons@gmail.com>
2024-06-13 02:44:13 +0900

    77. Day 5 - Creating our own Composables and Understanding Column

--- app/src/main/java/com/gery/unitconverter/MainActivity.kt
+++ app/src/main/java/com/gery/unitconverter/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -26,6 +28,17 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun UnitConverter() {
+    Column {
+        // Here all the UI elements will be stacked below each other
+        Row {
+            Greeting(name = "Android")
+            Greeting(name = "Android")
         }
     }
 }
