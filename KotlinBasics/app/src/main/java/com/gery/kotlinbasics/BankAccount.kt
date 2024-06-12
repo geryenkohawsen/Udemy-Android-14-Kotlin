29bf52dcba3655cabcc72df90d5c87fe294b0732
Geryenko Hawsen <gery.zarons@gmail.com>
2024-06-13 00:28:29 +0900

    71. Day 4 - BankAccountApp - Implementing the functionality

--- KotlinBasics/app/src/main/java/com/gery/kotlinbasics/BankAccount.kt
+++ KotlinBasics/app/src/main/java/com/gery/kotlinbasics/BankAccount.kt
@@ -1,18 +1,31 @@
 package com.gery.kotlinbasics
 
-class BankAccount(var accountHolder: String, var balance: Double) {
+class BankAccount(
+    var accountHolder: String,
+    var balance: Double
+) {
     private val transactionHistory = mutableListOf<String>()
 
     fun deposit(amount: Double) {
-
+        balance += amount
+        transactionHistory.add("$accountHolder deposited $$amount")
     }
 
     fun withdraw(amount: Double) {
-
+        if (amount <= balance) {
+            balance -= amount
+            transactionHistory.add("$accountHolder withdrew $$amount")
+        } else {
+            println("You don't have enough fund to withdraw $$amount")
+        }
     }
 
     fun displayTransactionHistory() {
-
+        println("Transaction history for $accountHolder")
+        for (transaction in transactionHistory) {
+            println(transaction)
+        }
+        println("$accountHolder's balance is $$balance")
     }
 
 }
--- KotlinBasics/app/src/main/java/com/gery/kotlinbasics/BankMain.kt
+++ KotlinBasics/app/src/main/java/com/gery/kotlinbasics/BankMain.kt
@@ -4,5 +4,11 @@ fun main() {
 
     val denisesBankAccount = BankAccount("Denis Putra", 1338.20)
 
-    println(denisesBankAccount.accountHolder)
+    denisesBankAccount.deposit(200.00)
+    denisesBankAccount.withdraw(1200.00)
+    denisesBankAccount.deposit(3000.00)
+    denisesBankAccount.deposit(2000.00)
+    denisesBankAccount.withdraw(3333.15)
+
+    denisesBankAccount.displayTransactionHistory()
 }
