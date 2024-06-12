9abbeb0770be65b2265afc3b3e6409fb06e19fbe
Geryenko Hawsen <gery.zarons@gmail.com>
2024-06-13 00:06:15 +0900

    70. Day 4 - BankAccountApp - Private Keyword and Defining List Type

---
+++ KotlinBasics/app/src/main/java/com/gery/kotlinbasics/BankAccount.kt
@@ -0,0 +1,18 @@
+package com.gery.kotlinbasics
+
+class BankAccount(var accountHolder: String, var balance: Double) {
+    private val transactionHistory = mutableListOf<String>()
+
+    fun deposit(amount: Double) {
+
+    }
+
+    fun withdraw(amount: Double) {
+
+    }
+
+    fun displayTransactionHistory() {
+
+    }
+
+}
---
+++ KotlinBasics/app/src/main/java/com/gery/kotlinbasics/BankMain.kt
@@ -0,0 +1,8 @@
+package com.gery.kotlinbasics
+
+fun main() {
+
+    val denisesBankAccount = BankAccount("Denis Putra", 1338.20)
+
+    println(denisesBankAccount.accountHolder)
+}
