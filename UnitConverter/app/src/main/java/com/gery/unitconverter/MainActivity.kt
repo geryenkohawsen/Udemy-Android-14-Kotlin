3b78cab091e2439812aae56b0a6a89e89d84f6c4
Geryenko Hawsen <gery.zarons@gmail.com>
2024-06-14 03:04:34 +0900

    82. Day 5 - Modifier, VerticalArrangement and HorizontalAlignment

--- UnitConverter/app/src/main/java/com/gery/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/com/gery/unitconverter/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,6 +19,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,7 +40,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter() {
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         // Here all the UI elements will be stacked below each other
         Text(text = "Unit Converter")
         OutlinedTextField(value = "", onValueChange = {
