bf309c91683e65b53037df2296be3fd1336476c6
Geryenko Hawsen <gery.zarons@gmail.com>
2024-06-14 03:17:22 +0900

    83. Day 5 - Spacer vs Padding

--- UnitConverter/app/src/main/java/com/gery/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/com/gery/unitconverter/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -23,6 +25,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gery.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,10 +50,11 @@ fun UnitConverter() {
     ) {
         // Here all the UI elements will be stacked below each other
         Text(text = "Unit Converter")
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(value = "", onValueChange = {
 
         })
-
+        Spacer(modifier = Modifier.height(16.dp))
         Row {
             Box {
                 Button(onClick = { /*TODO*/ }) {
@@ -71,6 +75,7 @@ fun UnitConverter() {
                 }
             }
         }
+        Spacer(modifier = Modifier.height(16.dp))
         Text(text = "Result:")
     }
 }
