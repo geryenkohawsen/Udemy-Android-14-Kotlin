fe535186750e75bd2706ba8497749784127dda39
Geryenko Hawsen <gery.zarons@gmail.com>
2024-06-15 11:50:58 +0900

    Quiz 17: Coding Exercise: Enhancing the Captain Game with Additional Features

--- CaptainGame/app/src/main/java/com/gery/myapplication/MainActivity.kt
+++ CaptainGame/app/src/main/java/com/gery/myapplication/MainActivity.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.gery.myapplication.ui.theme.MyApplicationTheme
 import kotlin.random.Random
@@ -36,18 +38,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CaptainGame(modifier: Modifier) {
-    val treasuredFound = remember { mutableStateOf(0) }
+    val treasuredFound = remember { mutableIntStateOf(0) }
     val direction = remember { mutableStateOf("North") }
+    val stormOrTreasure = remember { mutableStateOf("") }
 
-    Column {
-        Text(text = "Treasure Found: ${treasuredFound.value}")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Treasure Found: ${treasuredFound.intValue}")
         Text(text = "Current Direction: ${direction.value}")
         Button(onClick = {
             direction.value = "East"
 
             // 50/50 randomizer
             if (Random.nextBoolean()) {
-                treasuredFound.value += 1
+                treasuredFound.intValue += 1
+                stormOrTreasure.value = "Found a Treasure!"
+            } else {
+                stormOrTreasure.value = "Brace for the Storm!"
             }
         }) {
             Text(text = "Sail East")
@@ -57,7 +67,9 @@ fun CaptainGame(modifier: Modifier) {
 
             // 50/50 randomizer
             if (Random.nextBoolean()) {
-                treasuredFound.value += 1
+                treasuredFound.intValue += 1
+            } else {
+                stormOrTreasure.value = "Brace for the Storm!"
             }
         }) {
             Text(text = "Sail West")
@@ -67,7 +79,9 @@ fun CaptainGame(modifier: Modifier) {
 
             // 50/50 randomizer
             if (Random.nextBoolean()) {
-                treasuredFound.value += 1
+                treasuredFound.intValue += 1
+            } else {
+                stormOrTreasure.value = "Brace for the Storm!"
             }
         }) {
             Text(text = "Sail North")
@@ -77,10 +91,13 @@ fun CaptainGame(modifier: Modifier) {
 
             // 50/50 randomizer
             if (Random.nextBoolean()) {
-                treasuredFound.value += 1
+                treasuredFound.intValue += 1
+            } else {
+                stormOrTreasure.value = "Brace for the Storm!"
             }
         }) {
             Text(text = "Sail South")
         }
+        Text(text = stormOrTreasure.value)
     }
 }
