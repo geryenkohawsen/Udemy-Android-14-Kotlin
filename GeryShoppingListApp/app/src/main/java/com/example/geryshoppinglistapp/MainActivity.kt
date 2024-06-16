3004dcaf95a55362bb99cc1e3d48bda1c94faabc
Geryenko Hawsen <gery.zarons@gmail.com>
2024-06-17 00:25:42 +0900

    104. Day 7 - Adding the LazyColumn and the Data Class

--- GeryShoppingListApp/app/src/main/java/com/example/geryshoppinglistapp/MainActivity.kt
+++ GeryShoppingListApp/app/src/main/java/com/example/geryshoppinglistapp/MainActivity.kt
@@ -8,11 +8,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.geryshoppinglistapp.ui.theme.GeryShoppingListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,8 +29,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             GeryShoppingListAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    // stateItems
+                    var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
                     Column(
-                        modifier = Modifier.padding(innerPadding),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
                         verticalArrangement = Arrangement.Center
                     ) {
                         Button(
@@ -32,9 +44,22 @@ class MainActivity : ComponentActivity() {
                         ) {
                             Text(text = "Add Item")
                         }
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp)
+                        ) {
+                            items(sItems) {
+
+                            }
+                        }
                     }
                 }
             }
         }
     }
+}
+
+data class ShoppingItem(val id: Int, var name: String, var quantity: Int, var isEditing: Boolean) {
+
 }
