9c29dd3d9e2c1e6be7c60dde98c3b683a1095ab7
Geryenko Hawsen <gery.zarons@gmail.com>
2024-06-16 23:52:45 +0900

    102. Day 7 - Starting with the Shopping List UI by adding a button

---
+++ GeryShoppingListApp/app/src/main/java/com/example/geryshoppinglistapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.geryshoppinglistapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.geryshoppinglistapp.ui.theme.GeryShoppingListAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GeryShoppingListAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier.padding(innerPadding),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Button(
+                            modifier = Modifier.align(Alignment.CenterHorizontally),
+                            onClick = { /*TODO*/ }
+                        ) {
+                            Text(text = "Add Item")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
