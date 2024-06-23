295705f96f9ec05751d20394aa79b5907afc591a
Geryenko Hawsen <gery.zarons@gmail.com>
2024-06-23 14:07:09 +0900

    114. Day 7 - Copy Method

--- GeryShoppingListApp/app/src/main/java/com/example/geryshoppinglistapp/Basics.kt
+++ GeryShoppingListApp/app/src/main/java/com/example/geryshoppinglistapp/Basics.kt
@@ -4,4 +4,12 @@ fun main() {
     val numbers = listOf(1,2,3)
     val doubled = numbers.map{it * 2}
     println(doubled)
+
+    val blueRoseVase = Vase(color = "Blue", design = "Rose")
+    val redRoseVase = blueRoseVase.copy(color = "Red")
+
+    println(blueRoseVase)
+    println(redRoseVase)
 }
+
+data class Vase(val color: String, val design: String)
