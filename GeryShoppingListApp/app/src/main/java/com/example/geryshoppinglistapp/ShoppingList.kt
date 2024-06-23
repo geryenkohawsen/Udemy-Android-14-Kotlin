d39cb920bf54ad061d6534eb7b3cc58a26d535b0
Geryenko Hawsen <gery.zarons@gmail.com>
2024-06-23 12:52:00 +0900

    109. Day 7 - Setting up how a listitem should look like

--- GeryShoppingListApp/app/src/main/java/com/example/geryshoppinglistapp/ShoppingList.kt
+++ GeryShoppingListApp/app/src/main/java/com/example/geryshoppinglistapp/ShoppingList.kt
@@ -1,5 +1,7 @@
 package com.example.geryshoppinglistapp
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem(
@@ -53,7 +57,7 @@ fun ShoppingListApp(modifier: Modifier = Modifier) {
                 .padding(16.dp)
         ) {
             items(sItems) {
-
+                ShoppingListItem(item = it, onEditClick = { }, onDeleteClick = {})
             }
         }
 
@@ -119,5 +123,25 @@ fun ShoppingListApp(modifier: Modifier = Modifier) {
                 }
             )
         }
+    }
+}
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit,
+
+    ) {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .border(
+                border = BorderStroke(2.dp, Color(0XFF018786)),
+                shape = RoundedCornerShape(20)
+            )
+    ) {
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
     }
 }
