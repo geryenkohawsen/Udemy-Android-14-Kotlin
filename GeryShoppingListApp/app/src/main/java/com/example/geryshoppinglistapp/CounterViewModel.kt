7780184ad77e72080f2db3fbb8ca39e2cf61b2a0
Geryenko Hawsen <gery.zarons@gmail.com>
2024-06-30 00:11:11 +0900

    123. Day 8 - Converting Counter App with MVVM

---
+++ GeryShoppingListApp/app/src/main/java/com/example/geryshoppinglistapp/CounterViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.geryshoppinglistapp
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.lifecycle.ViewModel
+
+class CounterViewModel: ViewModel() {
+    private val _count = mutableIntStateOf(0)
+
+    // Expose the count as an immutable state
+    val count: MutableState<Int> = _count
+
+    fun increment() {
+        _count.intValue++
+    }
+
+    fun decrement() {
+        _count.intValue--
+    }
+}
--- GeryShoppingListApp/app/src/main/java/com/example/geryshoppinglistapp/MainActivity.kt
+++ GeryShoppingListApp/app/src/main/java/com/example/geryshoppinglistapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,16 +12,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -33,11 +28,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val viewModel: CounterViewModel by viewModels()
+
             GeryShoppingListAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TheCounterApp(modifier = Modifier
-                        .fillMaxSize()
-                        .padding(innerPadding))
+                    TheCounterApp(
+                        viewModel = viewModel,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    )
                 }
             }
         }
@@ -45,35 +45,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TheCounterApp(modifier: Modifier) {
-    val count = remember {
-        mutableStateOf(0)
-    }
-
-    fun increment() {
-        count.value++
-    }
-
-    fun decrement() {
-        count.value--
-    }
-
+fun TheCounterApp(viewModel: CounterViewModel, modifier: Modifier) {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "Count: ${count.value}",
+            text = "Count: ${viewModel.count.value}",
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold
         )
         Spacer(modifier = Modifier.height(16.dp))
         Row {
-            Button(onClick = { increment() }) {
+            Button(onClick = { viewModel.increment() }) {
                 Text(text = "Increment")
             }
-            Button(onClick = { decrement() }) {
+            Button(onClick = { viewModel.decrement() }) {
                 Text(text = "Decrement")
             }
         }
